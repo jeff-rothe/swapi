7b6caeaef70c1427bbd53923e1a97f9a2b880bfb
rothecreations@gmail.com <rothecreations@gmail.com>
2018-01-17 17:01:03 -0500

    Looping through and returning people names, closing out class for the day

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
-// The following is the shorthand for this example
-// $(document).ready(function())
+
 // Best practice to inluce this function, because it ensures the document is fully loaded
 
 $(() => {
-  
+// Longer form version of what is written below
+/*
+document.getElementById('#infoButton').addEventListener(() => {
+      
+    })
+*/
+$('#infoButton').click( () => {
+    $.ajax({
+        type: 'GET',
+        url: 'https://swapi.co/api/people'
+    }).done( (res) => {
+        let people = res.results;
+        for (person of people){
+            console.log(person.name);
+        }
+    })
+    })
 })
 
 
