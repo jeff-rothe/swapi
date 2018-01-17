1238c5b060ea04cb274fdd3fef9d38cc191f9cfc
rothecreations@gmail.com <rothecreations@gmail.com>
2018-01-17 16:21:07 -0500

    Initial commit of Star Wars API in class project (SWAPI)

---
+++ script.js
@@ -0,0 +1,9 @@
+// The following is the shorthand for this example
+// $(document).ready(function())
+// Best practice to inluce this function, because it ensures the document is fully loaded
+
+$(() => {
+    alert('Working!');
+})
+
+
